24a18b20943a8471587958c35611285be696dc1f
MarcusPaulJones <marcus.paul.jones97@gmail.com>
2019-01-27 08:46:17 +1100

    Partical System
    Created the Partical System

--- Assets/Scripts/ParticleEffect/LeafFallScript.cs
+++ Assets/Scripts/ParticleEffect/LeafFallScript.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class LeafFallScript : MonoBehaviour
 {
-    public ParticleSystem particles;
+    float timer;
+    public float spawnPerSecond;
+    //public ParticleSystem particles;
     public GameObject prefab;
+    public Vector3 spawnPos;
     public bool isActive;
+
+    public void SetActive(bool _A)
+    {
+        isActive = _A;
+    }
+    public void SetPosition(Vector3 _P)
+    {
+        spawnPos = _P;
+    }
     private void Update()
     {
         //particles 
         if(isActive)
         {
-
+            timer += Time.deltaTime;
+            if (timer >= (1 / spawnPerSecond)) 
+            {
+                timer = 0;
+                Instantiate(prefab, spawnPos + (Vector3.right * (Random.Range(-10, 11) / 10.0f)), Quaternion.Euler(0, 0, 0));
+            }
         }
     }
 }
