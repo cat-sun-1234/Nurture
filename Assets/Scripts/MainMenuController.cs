c9f2bea9f96e46dd24eb1c79481f117ae2237f16
Da Wei David Wang <dwthedaydreamer5@gmail.com>
2019-01-27 10:48:19 +1100

    Implmented restart when plant withered

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-﻿using System.Collections;
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -73,79 +73,3 @@ public class MainMenuController : MonoBehaviour
         }
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class MainMenuController : MonoBehaviour
-{
-    private int KanoTransformations = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-    #region Scene Data
-    string PPN;
-    int PPV;
-    public void SetPlayerPrefName(string _N)
-    {
-        PPN = _N;
-    }
-    public void SetPlayerPrefValue(int _V)
-    {
-        PPV = _V;
-    }
-    public void SetPlayerPref()
-    {
-        PlayerPrefs.SetInt(PPN, PPV);
-        Debug.Log(PPN + ": " + PPV);
-    }
-    #endregion
-    public void ActivateObject(GameObject _G)
-    {
-        _G.SetActive(true);
-    }
-    public void DeactivateObject(GameObject _G)
-    {
-        _G.SetActive(false);
-    }
-
-    public void GoToSelection()
-    {
-        SceneManager.LoadScene("SelectionScene");
-    }
-    public void SwitchToGame()
-    {
-        //TODO change scene name once the gameplay loop scene is finalised
-        //GetComponentInChildren<AudioSource>().Play();
-        SceneManager.LoadScene("Main");
-    }
-
-    public void SwitchToCredits()
-    {
-        //TODO implement credits screen
-        //GetComponentInChildren<AudioSource>().Play();
-        SceneManager.LoadScene("CreditsScreen");
-    }
-
-    public void SwitchToSplash()
-    {
-        //GetComponentInChildren<AudioSource>().Play();
-        SceneManager.LoadScene("SplashScreen");
-    }
-
-    public void DULLARD()
-    {
-        KanoTransformations++;
-
-        if (KanoTransformations == 15)
-        {
-            KanoTransformations = 0;
-            SceneManager.LoadScene("CreditsScreen 1");
-        }
-    }
-}
->>>>>>> 204fc37030876300754b73bb7e8e01bd72957468
