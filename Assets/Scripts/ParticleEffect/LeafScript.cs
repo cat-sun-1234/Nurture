3915e1052df5163a4161c9815a676ecb2154a16e
MarcusPaulJones <marcus.paul.jones97@gmail.com>
2019-01-27 15:40:20 +1100

    Particle Fix
    Particles Now Show up on Selection Screen

--- Assets/Scripts/ParticleEffect/LeafScript.cs
+++ Assets/Scripts/ParticleEffect/LeafScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class LeafScript : MonoBehaviour
 {
+    public float fallSpeed;
     float timer;
     float curDir;
     private void Awake()
@@ -16,7 +17,7 @@ public class LeafScript : MonoBehaviour
         transform.Rotate(Vector3.forward, Random.Range(-1, 1));
         curDir = Mathf.Clamp(curDir + (Random.Range(-5, 6) / 10.0f), -1.5f, 1.5f);
         //Debug.Log((Vector3.down + (Vector3.right * curDir)));
-        gameObject.GetComponent<Rigidbody2D>().MovePosition(transform.position + (Vector3.down + (Vector3.right * (curDir))) * 2.0f * Time.deltaTime);
+        gameObject.GetComponent<Rigidbody2D>().MovePosition(transform.position + (Vector3.down + (Vector3.right * (curDir))) * fallSpeed * Time.deltaTime);
         timer += Time.deltaTime;
         transform.localScale = (Vector3.one * ((5 - timer) / 5.0f));
         if (timer > 5)
